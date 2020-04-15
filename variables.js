9c029ba790cf1e717ef94191142c554e16d5c0a4
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-04-14 23:00:21 -0500

    Variables en js

---
+++ variables.js
@@ -0,0 +1,23 @@
+var nombre = "oscar";
+
+/* "var" es una palabra reservada y representa un espacio en memoria */
+/* el espacio en memoria se va a llamar "nombre"*/
+/* el valor de la variables es "oscar" */
+
+//-----------------------------------------------------
+
+/* declar una variable */
+var edad;
+/* inicializar una variable */
+edad = 27;
+
+// -----------------------------------------------------
+/* variable de un conjunto de elementos */
+var elementos = ["PC", "Celular"]
+/* ↑ declarar e inicializar la variable al mismo tiempo↑ */
+
+/* declaracion de un objeto */
+var persona = {
+  nombre = "Sebastian",
+  edad = 28
+}
