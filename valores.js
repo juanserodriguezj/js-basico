000f0526e20f517abab54219b9c7655de586161f
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-04-14 22:37:49 -0500

    Valores en js

---
+++ valores.js
@@ -0,0 +1,19 @@
+/* numeros */
+40
+.4
+-4
+4
+/* texto */
+"Juan Sebastian Rodriguez"
+'a'
+'b'
+/* Boolean */
+true
+false
+/* valores vacios */
+null
+undefined
+/* ↑↑↑↑↑↑↑↑ Valores primitivos ↑↑↑↑↑↑↑↑↑↑  */
+/* ↓↓↓↓↓↓↓↓ Valores no primitivosobjeto ↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
+[1,2,3,4,5]
+{nombre: "Diego"}
