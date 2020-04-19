3627c1f8b26400a12425c105aea1b374788196d3
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-04-19 04:45:01 -0500

    Resuelto el juego piedra-papel-tijeras con javascript

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+/* Cuando variables y funciones se procesan antes que se ejecute cualquier tipo de codigo */
+// var miNombre;       //Declaracion de variable
+// minombre = "Juan";  //Inicializa la varable
+
+// Se puede hacer en la misma lina.
+// var minombre = "Juan";
+
+// hoisting mandamos a llamar variables sin haberlas declarado.
+console.log(minombre);
+
+var minombre = "Juan";
+// El resultado es undefined, usando el jep compiler
+var minombre = undefined;
+console.log(minombre);
