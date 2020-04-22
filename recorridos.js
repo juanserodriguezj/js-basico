b1dad74b28d9342164e0bdab5c4e41a7ddd449a9
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-04-21 22:51:17 -0500

    reccoridos con find(), forEach() y some() en javascript

--- recorridos.js
+++ recorridos.js
@@ -28,3 +28,43 @@ var costoArticulos = articulos.map(function (elemento) {
 
 console.log(nombreArticulos);
 console.log(costoArticulos);
+
+/* Mas recorridos en el array */
+
+//Javascript te permite crear arrays de objetos
+var cumpleannos = [
+  { persona: "Luna", dia: 13 },
+  { persona: "Mia", dia: 8 },
+  { persona: "Valery", dia: 19 },
+  { persona: "Arturo", dia: 3 },
+  { persona: "Marisol", dia: 3 },
+  { persona: "Leo", dia: 18 },
+  { persona: "Leslie", dia: 24 },
+  { persona: "Mia", dia: 22 },
+  { persona: "Jose", dia: 31 },
+  { persona: "Mia", dia: 4 },
+];
+/* METODO FILTER Crea un nuevo arreglo y con la funcion pasando como parametro cada objeto, regresa al arreglo que se esta creando
+los objetos que cumplen con la condicion en el return*/
+var cumpleannosPosterioresA10 = cumpleannos.filter(function (cumpleanno) {
+  return cumpleanno.dia >= 10;
+});
+/* METODO MAP Crea un nuevo arreglo ya no con objetos, sino con los valores del valor que necesitamos */
+var nombresCumpleanneros = cumpleannos.map(function (nombreCumpleannero) {
+  return nombreCumpleannero.persona;
+});
+/*METODO FIND Sirve para buscar dentro de un arreglo en este caso dentro de varios objetos el valor buscado y 
+crea un nuevo arreglo con la busqueda, el detalle es que al parecer solo retorna el primer valor encontrado, en caso
+que este se encuentre duplicado */
+var buscarCumpleannero = cumpleannos.find(function (cumpleannero) {
+  return cumpleannero.persona === "Mia";
+});
+/*METODO FOREACH A diferencia de los anteriores no genenra un arreglo solo obtiene los valores indicados en el arreglo*/
+cumpleannos.forEach(function (cumpleanno) {
+  console.log(cumpleanno.persona);
+});
+
+/* METODO SOME Te permite validar mediante un true o false si se cumple una condicion dentro de tus arreglos */
+var nombreMia = cumpleannos.some(function (nombre) {
+  return nombre.persona === "Mia";
+});
