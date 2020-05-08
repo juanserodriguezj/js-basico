3db1b1f4e0014280e1f2cb2d75c3d378c100aab1
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-05-08 18:57:39 -0500

    Async-await en js

---
+++ Async-await/asyncAwait.js
@@ -0,0 +1,39 @@
+const API_URL = "https://swapi.dev/api/";
+const PEOPLE_URL = "people/:id";
+const opts = { crossDomain: true };
+
+function obtenerPersonaje(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+    $.get(url, opts, function (data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
+}
+
+function onError(id) {
+  console.log(`Sucedio un error al obtener el personaje ${id}`);
+}
+
+function mostrarPersonaje(personajes) {
+  for (let personaje of personajes) {
+    console.log(`Hola, yo soy ${personaje.name}`);
+  }
+}
+
+/* Async-await es la manera más simple y clara de realizar tareas asíncronas. Await detiene la ejecución del programa hasta que todas las promesas sean resueltas. Para poder utilizar esta forma, hay que colocar async antes de la definición de la función, y encerrar el llamado a Promises.all() dentro de un bloque try … catch.
+ */
+
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  var promesas = ids.map((id) => obtenerPersonaje(id));
+
+  try {
+    var personajes = await Promise.all(promesas);
+    console.log(personajes);
+  } catch (id) {
+    onError(id);
+  }
+}
+
+obtenerPersonajes();
